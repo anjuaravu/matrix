62abb14d93ca8a08f1b46266436b753e89c702e8
anjuaravu <98830774+anjuaravu@users.noreply.github.com>
2023-09-27 19:06:13 +0530

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,17 @@
+import re
+
+first_multiple_input = input().rstrip().split()
+n = int(first_multiple_input[0])
+m = int(first_multiple_input[1])
+matrix = []
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+
+matrix = list(zip(*matrix))
+sample = str()
+for i in matrix:
+    for j in i:
+        sample += j
+print(re.sub(r'(?<=\w)([^\w\d]+)(?=\w)', ' ', sample))
+
